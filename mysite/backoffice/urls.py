32f13594a4158e43447951620428a49a3e3ee19f
fredonra <fede_99_leiva@hotmail.com>
2024-09-25 00:38:02 -0300

    feat: agrego un redirect a conversaciones en caso de no ingresar entidad en la url

--- mysite/backoffice/urls.py
+++ mysite/backoffice/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.crud, name='home'),
     path('conversaciones/', views.crud, name='conversaciones'),
     path('gestiones/', views.crud, name='gestiones'),
     path('pasos/', views.crud, name='pasos'),
