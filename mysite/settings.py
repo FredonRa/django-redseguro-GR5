26b4a0b4be8a4d84a8656823e540561c62d25b6c
fredonra <fede_99_leiva@hotmail.com>
2024-10-05 19:26:20 -0300

    feat: agrego static_url y static_root

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,7 +139,8 @@ COMPRESS_JS_FILTERS = ['compressor.filters.jsmin.JSMinFilter']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/css/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
